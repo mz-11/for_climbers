a6d80d1992ceeab91e9f4b9891ccf17d5ff9872c
mz-11 <m.mizukoshi73@gmail.com>
2020-06-29 09:58:43 +0000

    create model validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 姓、名、メール、パスワードがあれば有効な状態であること
+  it "is valid with name, email, and password" do
+    user = User.new(
+      name: "Aaron",
+      email: "aaaa@aaaa.com",
+      password: "aaaa0000",
+      )
+      expect(user).to be_valid
+  end
+  
+  # 名がなければ無効な状態であること
+  it "is invalid without a name" do
+    user = User.new(name: nil)
+    user.valid? # valid?:ActiveRecordメソッドで、モデルオブジェクトのバリデーションが成功したかどうか真偽値で返す
+    expect(user.errors[:name]).to include("can't be blank") #toをnot_toにすると失敗の確認もできる
+  end
+  
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank") 
+  end
+  
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address" do
+    User.create(
+      name: "joe",
+      email: "test@example.com",
+      password: "example1234"
+    ) #テストの前にcreateでユーザーを保存
+    user = User.new(
+      name: "joe",
+      email: "test@example.com",
+      password: "example1234"
+    ) #2件目のユーザーをテスト対象のオブジェクトとしてインスタンス化した。
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+  
+  # 重複した名前なら無効な状態であること
+  it "is invalid with a duplicate name" do
+    User.create(
+      name: "tarou",
+      email: "test@example.com",
+      password: "example1234"
+    )
+    user = User.new(
+      name: "tarou",
+      email: "test@example.com",
+      password: "example1234"
+    )
+    user.valid?
+    expect(user.errors[:name]).to include("has already been taken")
+  end
+
+
 end
--- spec/user.rb
+++ spec/user.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
 
  factory :user do
-   nickname              {"taro"}
-   email                 {"kkk@gmail.com"}
-   password              {"00000000"}
-   password_confirmation {"00000000"}
+   # nickname              {"taro"}
+   # email                 {"kkk@gmail.com"}
+   # password              {"00000000"}
+   # password_confirmation {"00000000"}
  end
 end

46ca6a0f2f3c8c382f6fd144e88c52d400c27ec5
mz-11 <m.mizukoshi73@gmail.com>
2020-06-30 14:21:18 +0000

    add validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,61 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # 姓、名、メール、パスワードがあれば有効な状態であること
-  it "is valid with name, email, and password" do
-    user = User.new(
-      name: "Aaron",
-      email: "aaaa@aaaa.com",
-      password: "aaaa0000",
+  context "基本的なフォームのバリデーション" do
+    it "名前、メール、パスワードがあれば有効な状態であること" do
+      user = User.new(
+        name: "Aaron",
+        email: "aaaa@aaaa.com",
+        password: "aaaa0000",
+        )
+        expect(user).to be_valid
+    end
+    
+    it "名前がなければ無効な状態であること" do
+      user = User.new(name: nil)
+      user.valid? # valid?:ActiveRecordメソッドで、モデルオブジェクトのバリデーションが成功したかどうか真偽値で返す
+      expect(user.errors[:name]).to include("can't be blank") #toをnot_toにすると失敗の確認もできる
+    end
+    
+    it "メールアドレスがなければ無効な状態であること" do
+      user = User.new(email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank") 
+    end
+    
+    it "重複したメールアドレスなら無効な状態であること" do
+      User.create(
+        name: "joe",
+        email: "test@example.com",
+        password: "example1234"
+      ) #テストの前にcreateでユーザーを保存
+      user = User.new(
+        name: "joe",
+        email: "test@example.com",
+        password: "example1234"
+      ) #2件目のユーザーをテスト対象のオブジェクトとしてインスタンス化した。
+      user.valid?
+      expect(user.errors[:email]).to include("はすでに存在します")
+    end
+    
+    it "重複した名前なら無効な状態であること" do
+      User.create(
+        name: "tarou",
+        email: "test@example.com",
+        password: "example1234"
       )
-      expect(user).to be_valid
-  end
+      user = User.new(
+        name: "tarou",
+        email: "test@example.com",
+        password: "example1234"
+      )
+      user.valid?
+      expect(user.errors[:name]).to include("はすでに存在します")
+    end
   
-  # 名がなければ無効な状態であること
-  it "is invalid without a name" do
-    user = User.new(name: nil)
-    user.valid? # valid?:ActiveRecordメソッドで、モデルオブジェクトのバリデーションが成功したかどうか真偽値で返す
-    expect(user.errors[:name]).to include("can't be blank") #toをnot_toにすると失敗の確認もできる
+    it "名前の文字数が15文字以内であること" do
+      user = User.new(name: "a" * 16)
+      user.valid?
+      expect(user.errors[:name]).to include("は15文字以内で入力してください")
+    end
+     
+    # 正規表現のテスト
+    
+    # expect().to match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+    
+    # describe context を使用しスペックをDRYにする
   end
-  
-  # メールアドレスがなければ無効な状態であること
-  it "is invalid without an email address" do
-    user = User.new(email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank") 
-  end
-  
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
-    User.create(
-      name: "joe",
-      email: "test@example.com",
-      password: "example1234"
-    ) #テストの前にcreateでユーザーを保存
-    user = User.new(
-      name: "joe",
-      email: "test@example.com",
-      password: "example1234"
-    ) #2件目のユーザーをテスト対象のオブジェクトとしてインスタンス化した。
-    user.valid?
-    expect(user.errors[:email]).to include("has already been taken")
-  end
-  
-  # 重複した名前なら無効な状態であること
-  it "is invalid with a duplicate name" do
-    User.create(
-      name: "tarou",
-      email: "test@example.com",
-      password: "example1234"
-    )
-    user = User.new(
-      name: "tarou",
-      email: "test@example.com",
-      password: "example1234"
-    )
-    user.valid?
-    expect(user.errors[:name]).to include("has already been taken")
-  end
-
 
 end

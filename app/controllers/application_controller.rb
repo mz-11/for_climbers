954c42e91091a6a6d6d1b28b2455cdde8c62ed90
mz-11 <m.mizukoshi73@gmail.com>
2020-07-04 06:51:49 +0000

    add .rubocop_todo.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :login_required
   # pages,users_controllerにはskip_before_actionを記述
-  
-  
+
   helper_method :current_user
-  # ログインしているユーザーを取得する処理は頻繁に行うため、コントローラーやビューから簡単に呼べるようcurrent_userとして定義。さらにhelper_methodしていすることで全てのビューから使えるようにした。
+  # ログインユーザーを取得する処理は頻繁に行うため、コントローラーやビューから簡単に呼べるようcurrent_userとして定義
+  # さらにhelper_methodしていすることで全てのビューから使えるようにした。
   helper_method :logged_in?
   
   add_flash_types :success, :info, :warning, :danger
   # Bootstrapに対応した success info warning danger 4つのキーが使用できる
-  
+
   private
   def login_required
     if current_user == nil

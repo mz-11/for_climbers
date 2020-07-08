3a90e30906feb1a39b56acc67908a8c37e9951d1
mz-11 <m.mizukoshi73@gmail.com>
2020-07-08 14:19:39 +0000

    fix gym_name of post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/_318-9776.jpg')) }
-    gym_name { "B-Punp" }
+    gym_name { "B-Pump" }
     grade { "2級" }
     category { "power" }
     association :user
